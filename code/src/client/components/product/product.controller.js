a284dc85cab11f30a12ebe62c791be058d7ef92c
nelson.alcocer <nelson.alcocer@jalasoft.com>
2016-04-10 15:12:17 -0400

    Added validations for products form.

--- code/src/client/components/product/product.controller.js
+++ code/src/client/components/product/product.controller.js
@@ -3,43 +3,11 @@
 
     angular
         .module('app.company')
-        .controller('CompanyController', CompanyController);
+        .controller('ProductController', ProductController);
 
-    CompanyController.$inject = [ 'companyService', 'socket' ];
+    ProductController.$inject = [ 'companyService', 'socket' ];
 
-    function CompanyController(companyService, socket) {
-        var vm = this;
-        vm.item = {};
-        vm.items = getCompanies();
-        vm.save = save;
+    function ProductController(companyService, socket) {
 
-        function save() {
-            var promise = companyService.save(vm.item).$promise;
-            promise.then(function (data) {
-                vm.item = {};
-                console.log('item created', data);
-            }, function (error){
-                console.error(error);
-                // TODO remove code whe the REST API will done
-                //vm.items.push(vm.item);
-                vm.item = {};
-            });
-        }
-
-        function getCompanies() {
-            var promise = companyService.query().$promise;
-            promise.then(function (data) {
-                console.log('data', data);
-                vm.items = data;
-            }, function (error) {
-                //TODO: implement error handle and notification
-               console.log('error', error);
-            });
-        }
-
-        socket.on('companies:create', function(data){
-            console.log('socket:', data);
-            vm.items.push(data);
-        })
     }
 })();
